f5522952caca1898f14fe8a4be8d39421e174f26
Pedro <57961751+pedrozau@users.noreply.github.com>
2024-07-31 03:20:41 +0100

    add new changes

--- src/modules/service/service.service.ts
+++ src/modules/service/service.service.ts
@@ -60,13 +60,9 @@ export class ServiceService {
       const service = await this.getServiceById(serviceId)
       const user = await this.getUserById(userId)
         
-      if(service.price > user.balance)  {
+      if(service.price >= user.balance)  {
          throw new HttpException("Saldo insuficiente para contratar um serviço.",400)
-      }else {
-        
-          return true
-
-      } 
+      }
       
         
         
