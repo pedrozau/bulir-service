4cc23657a03319abb4993e121acb3cc4f7eeb26c
Pedro <57961751+pedrozau@users.noreply.github.com>
2024-08-01 16:02:23 +0100

    add new nodification transaction historic

--- src/modules/transaction/transaction.service.ts
+++ src/modules/transaction/transaction.service.ts
@@ -26,6 +26,24 @@ export class TransactionService {
         }
     } 
 
+      // Check if user is a client
+  async isClient(userId: string) {
+    const user = await this.prisma.user.findFirst({ where: { id: userId } });
+    if (!user) {
+      throw new HttpException('User not found', 404);
+    }
+    return user.role === 'cliente';
+  }
+
+  // Check if user is a provider
+  async isProvider(userId: string) {
+    const provider = await this.prisma.user.findFirst({ where: { id: userId } });
+    if (!provider) {
+      throw new HttpException('Provider not found', 404);
+    }
+    return provider.role === 'prestador';
+  }
+
 
 
     async getUserTransactions(userId: string) {
@@ -34,14 +52,32 @@ export class TransactionService {
           
             const {id} = await this.checkUser(userId)
 
+           const isClient = await this.isClient(userId)
+           const isProvider = await this.isProvider(userId)
+
+
+           if(isClient) {
+
             return await this.prisma.transaction.findMany({
                  include: {
-                    service: true, 
+                    service: true,
                  },
                  where: {
                      clientId: id
                  }
+              })
+           }
+
+           if(isProvider) {
+            return await this.prisma.transaction.findMany({
+                 include: {
+                    service: true,
+                 },
+                 where: {
+                     providerId: id
+                 }
             })
+           }
 
           }catch(error) {
 
